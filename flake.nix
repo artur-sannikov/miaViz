d620777d7ceb3eac182e9753e1b8306a6370ab71
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-10-14 10:33:08 +0300

    add: mia flake

--- flake.nix
+++ flake.nix
@@ -3,44 +3,54 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    mia-flake.url = "github:artur-sannikov/mia/nix-flakes";
   };
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
+      mia-flake,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        mia = mia-flake.packages.${system}.default;
         miaViz = pkgs.rPackages.buildRPackage {
           name = "miaViz";
           src = self;
-          propagatedBuildInputs = builtins.attrValues {
-            inherit (pkgs.rPackages)
-              ape
-              BiocGenerics
-              BiocParallel
-              DelayedArray
-              DirichletMultinomial
-              dplyr
-              ggnewscale
-              ggrepel
-              ggtree
-              purrr
-              rlang
-              S4Vectors
-              scater
-              SingleCellExperiment
-              tibble
-              tidygraph
-              tidyr
-              tidytext
-              tidytree
-              viridis
-              ;
-          };
+          propagatedBuildInputs =
+            builtins.attrValues {
+              inherit (pkgs.rPackages)
+                ape
+                BiocGenerics
+                BiocParallel
+                DelayedArray
+                DirichletMultinomial
+                dplyr
+                ggnewscale
+                ggrepel
+                ggtree
+                purrr
+                rlang
+                S4Vectors
+                scater
+                SingleCellExperiment
+                tibble
+                tidygraph
+                tidyr
+                tidytext
+                tidytree
+                viridis
+                TreeSummarizedExperiment
+                ggraph
+                ;
+            }
+            ++ [
+              # Install the latest version of mia
+              mia
+            ];
         };
       in
       {
